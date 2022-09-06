0b310781a391a3d1843a5594cb7d9e2bc501ed42
DeForestt <deforesttthompson@gmail.com>
2022-09-05 23:17:22 -0400

    add stdin box

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -11,6 +11,8 @@ interface EditorProps {
 const Editor : React.FC<EditorProps> = (props : EditorProps) => {
     const [code, setCode] = React.useState(props.defaultCode)
     const [output, setOutput] = React.useState("Output will be here");
+    const [mode, setMode] = React.useState("code");
+    const [stdin, setStdin] = React.useState("");
 
     const run = () => {
 
@@ -25,23 +27,36 @@ const Editor : React.FC<EditorProps> = (props : EditorProps) => {
         runBox(project).then(res => {setOutput(res.output);});
     };
 
+    const switchMode = () => {
+        mode === "code" ? setMode("console") : setMode("code");
+    };
+
+    const textBoxes = () => {
+        if (mode === "code") {
+            return <CodeEditor
+            value={code}
+            language="js"
+            placeholder="Please enter aflat code here."
+            onChange={(evn) => setCode(evn.target.value)}
+            padding={15}
+            style={{
+              fontSize: 12,
+              backgroundColor: "#f5f5f5",
+              fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+              width: "20rem",
+              height: "20rem",
+            }}
+          />
+        } else {
+             return <textarea onChange={(evn) => setStdin(evn.target.value)}></textarea>
+        }
+    }
+
     return (<>
-        <CodeEditor
-          value={code}
-          language="js"
-          placeholder="Please enter aflat code here."
-          onChange={(evn) => setCode(evn.target.value)}
-          padding={15}
-          style={{
-            fontSize: 12,
-            backgroundColor: "#f5f5f5",
-            fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-            width: "20rem",
-            height: "20rem",
-          }}
-        />
-        <button onClick={() => run()}>Run</button>
-        <ConsoleEmulator text={output} />
+        <button onClick={switchMode}>{mode == "code" ? "stdin" : "code"}</button>
+        {textBoxes()}
+        <button onClick={run}>Run</button>
+        <ConsoleEmulator text={output}/>
 
     </>)
 }
